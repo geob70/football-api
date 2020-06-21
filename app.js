4da400f69436e97bdda7806e4aa7cdb0666bdb88
George Sylvester Olamide <geoslyman57@gmail.com>
2020-06-21 19:06:19 +0100

    cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors = require("cors");
+// const cors = require("cors");
 require("dotenv").config();
 const { mongourl } = require("./config/keys");
 const app = express();
@@ -16,7 +16,7 @@ mongoose.connect(mongourl, {
   useCreateIndex: true,
 });
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -27,11 +27,22 @@ app.use(bodyParser.json());
 //     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
 //   );
 //   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET, OPTIONS");
 //     return res.status(200).json({});
 //   }
 //   next();
 // });
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 //Route
 app.use("/teams", teamRoute);
